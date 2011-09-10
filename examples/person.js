0188ad6b27bce35c5596ad083577c8da0293605c
bcoe <bencoe@gmail.com>
2011-09-10 05:55:09 -0400

    Had to fix a few things to make the Inception use-case work properl, i.e., using sexy-args in your sexy-args.

--- examples/person.js
+++ examples/person.js
@@ -1,25 +1,41 @@
 var sexy = require('sexy-args'),
     puts = require('sys').puts;
 
-function extend(obj1, obj2) {
-    for (var key in obj2) {
-        if (obj2.hasOwnProperty(key)) {
-            obj1[key] = obj2[key];
+function extend(obj1, obj2, obj3, terminal) {
+    sexy.args([this, 'object1', 'object2', ['object3', 'boolean1'], 'boolean1'], function() {
+        if (!terminal) {
+           extend(obj2, obj3, true);
+           extend(obj1, obj2, true);
+        } else {
+            for (var key in obj2) {
+                if (obj2.hasOwnProperty(key)) {
+                    obj1[key] = obj2[key];
+                }
+            }
         }
-    }
+    });
 }
 
 function Person(attributes) {
-    sexy.args([this, 'object1'], function() {
-        extend(this, attributes);
-    });
+    sexy.args(
+        [this, 'object1'], 
+        {
+            'object1': {
+                'alive': true
+            }
+        },
+        function() {
+            this.transportation = 'walking';
+            extend(this, attributes);
+        }
+    );
 }
 
 Person.prototype.updateInformation = function(name, age, attributes) {
     sexy.args([this, ['string1', 'number1', 'object1'], ['number1', 'object1'], 'object1'], function() {
         this.age = age || this.age;
         this.name = name || this.name;
-        extend(this, attributes);
+        extend(this, attributes, {'Does he like turtles?': true});
     });
 };
 
