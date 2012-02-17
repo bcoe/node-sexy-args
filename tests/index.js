f7eb855cf866e37a0dd92ec9e907dd08ca589be5
bcoe <bencoe@gmail.com>
2012-02-17 13:40:16 -0800

    Fixed up spacing. Added extend functionality.

--- tests/index.js
+++ tests/index.js
@@ -1,31 +1,31 @@
 var parserTests = require('./test_parser'),
-    puts = require('sys').puts,
-    tests = [];
+	puts = require('util').puts,
+	tests = [];
     
 function run(callback, test) {
-    callback(
-        function() {
-            puts(test + ' \033[32m[Success]\033[m');
-            if (tests.length == 0) {
-                puts(' \033[32mAll tests finished.\033[m');
-                process.exit();
-            }
-        
-            var nextTest = tests.shift();
-            nextTest();
-        },
-        test + ': '
-    );
+	callback(
+		function() {
+			puts(test + ' \033[32m[Success]\033[m');
+			if (tests.length == 0) {
+				puts(' \033[32mAll tests finished.\033[m');
+				process.exit();
+			}
+			
+			var nextTest = tests.shift();
+			nextTest();
+		},
+		test + ': '
+	);
 }
 
 function addTests(testsObject) {
-    for (var test in testsObject) {
-        (function(func, name) {
-            tests.push(function() {
-                run(func, name);
-            });
-        })(testsObject[test], test);
-    }
+	for (var test in testsObject) {
+		(function(func, name) {
+			tests.push(function() {
+				run(func, name);
+			});
+		})(testsObject[test], test);
+	}
 }
 
 addTests(parserTests.tests);
